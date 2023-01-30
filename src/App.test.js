f61eb801efb7911178485612f6f83c290a486f34
olistopad <olistopad@xbsoftware.com>
2023-01-30 18:04:24 +0300

    [dev] correct imports App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const root = ReactDOMClient.createRoot(div);
+  root.render(<App />)
 });
